5d580f027cce3657754ebbed97ecc52447cd5f41
Austin Andrews <admin@templarian.com>
2025-05-31 10:36:31 -0500

    Create element.forEach.test.ts

---
+++ src/element.forEach.test.ts
@@ -0,0 +1,76 @@
+import { describe, expect, test, beforeEach, afterEach } from '@jest/globals';
+
+import {
+  Component, Prop, Part,
+  getProps, selectComponent,
+  forEach
+} from "./element";
+
+describe("forEach", () => {
+
+  const HELLO_ITEM = 'hello-item';
+
+  @Component({
+    selector: HELLO_ITEM,
+    template: '<span part="count"></span>'
+  })
+  class HelloItem extends HTMLElement {
+    @Prop() count: number = 0;
+    @Part() $count: HTMLSpanElement;
+
+    render() {
+      this.$count.textContent = `${this.count}`;
+    }
+  }
+
+  const HELLO_WORLD = 'hello-world';
+
+  @Component({
+    selector: HELLO_WORLD,
+    template: '<div part="list"></div>'
+  })
+  class HelloWorld extends HTMLElement {
+    @Prop() items: any[] = [];
+    @Part() $list: HTMLDivElement;
+
+    connectedCallback() {
+      forEach({
+        container: this.$list,
+        items: this.items,
+        type() {
+          return HelloItem;
+        }
+      })
+    }
+  }
+
+  beforeEach(() => {
+    var c = document.createElement(HELLO_WORLD);
+    document.body.appendChild(c);
+  });
+
+  afterEach(() => {
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  test('should be registered', () => {
+    expect(customElements.get(HELLO_ITEM)).toBeDefined();
+    expect(customElements.get(HELLO_WORLD)).toBeDefined();
+  });
+
+  test("initial render", () => {
+    const component = selectComponent<HelloWorld>(HELLO_WORLD);
+    const { $list } = component;
+    expect($list.children.length).toBe(0);
+  });
+
+  test("items.push", () => {
+    const component = selectComponent<HelloWorld>(HELLO_WORLD);
+    const { $list } = component;
+    component.items.push({ count: 1 });
+    expect($list.children.length).toBe(1);
+  });
+
+});
