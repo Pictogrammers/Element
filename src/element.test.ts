c5e058064afd8d2790b80f4f90ad3d024e46c9e7
Austin Andrews <admin@templarian.com>
2025-05-31 10:07:45 -0500

    basic test

--- src/element.test.ts
+++ src/element.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test, jest } from '@jest/globals';
 
-import { Component, Prop } from "./element"
+import { Component, Prop, Part } from "./element"
 
 describe("createElement", () => {
 
@@ -10,10 +10,28 @@ describe("createElement", () => {
   })
   class HelloWorld extends HTMLElement {
     @Prop() count: number = 0;
+    @Part() $count: HTMLSpanElement;
+
+    render() {
+      this.$count.textContent = `${this.count}`;
+    }
   }
 
   test("basic", () => {
     const ele = document.createElement('hello-world') as HelloWorld;
+    document.body.append(ele);
     expect(ele.count).toBe(0);
+    expect(ele.shadowRoot?.innerHTML).toBe('<span part="count">0</span>');
+    ele.remove();
   });
+
+  test("basic template render", () => {
+    const ele = document.createElement('hello-world') as HelloWorld;
+    document.body.append(ele);
+    ele.count = 1;
+    expect(ele.count).toBe(1);
+    expect(ele.shadowRoot?.innerHTML).toBe('<span part="count">1</span>');
+    ele.remove();
+  });
+
 });
